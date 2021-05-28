5bd7d7a30bb5ca57d614eab59dcc6b985717502e
Jakob Richter <code@jakob-r.de>
2021-05-28 13:32:59 +0200

    add paper rev

--- benchmark/_parameters.R
+++ benchmark/_parameters.R
@@ -30,7 +30,8 @@ if (TESTMODE) {
     #saturation = {x = c(0,5,7,7.8,8)/10; list(early = x, final = x/4)},
     sigmoid = {x = c(0,1,2,7,8)/10; list(early = x, final = x/4)},
     #paper_mod = list(early = c(0,0.68,0.82,0.93,0.93), final = c(0,0.13,0.17,0.23,0.20)),
-    paper2 = list(early = c(0,0.68,0.82,0.95,0.91), final = 2*c(0,0.13,0.17,0.23,0.20))# ,
+    paper2 = list(early = c(0,0.68,0.82,0.95,0.91), final = 2*c(0,0.13,0.17,0.23,0.20)),
     #paper_mod2 = list(early = c(0,0.68,0.82,0.93,0.93), final = 2*c(0,0.13,0.17,0.23,0.20))
+    paper_rev = list(early = c(0,0.13,0.17,0.23,0.20), final = c(0,0.68,0.82,0.95,0.91))
   )
 }
